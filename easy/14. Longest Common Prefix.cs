633946339645dd71d115b0136b1058bdf5ca66a3
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-06-28 01:20:19 +0800

    Add files via upload

---
+++ easy/14. Longest Common Prefix.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        if (strs.Length == 0)
+        {
+            return "";
+        }
+        else if (strs.Length == 1)
+        {
+            return strs[0];
+        }
+        else
+        {
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length)
+                    {
+                        return i == 0 ? "" : strs[0].Substring(0, i);
+                    }
+                    else if (strs[0][i] != strs[j][i])
+                    {
+                        return i == 0 ? "" : strs[0].Substring(0, i);
+                    }
+                }
+            }
+            return strs[0];
+        }
+    }
+}
