3a1a22f4d375aaec57a150a3c0530d22a910388c
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-06-28 01:29:47 +0800

    Add files via upload

---
+++ easy/1. Two Sum.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.Length; i++)
+            for (int j = i + 1; j < nums.Length; j++)
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
+        return result;
+    }
+}
