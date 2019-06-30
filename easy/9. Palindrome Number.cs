543cfba5841676b4e8b6b764322907af7eca7720
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-06-30 17:10:45 +0800

    Add files via upload

---
+++ easy/9. Palindrome Number.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+        else if (x == 0)
+            return true;
+        else
+        {
+            string xToStr = x.ToString();
+            int lenx = xToStr.Length;
+            for (int i = 0; i < lenx / 2; i++)
+            {
+                if (xToStr[i] != xToStr[lenx - i - 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
