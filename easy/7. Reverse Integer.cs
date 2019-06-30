2e1dd865fb77f323809ddc2755371d46571650bb
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-06-30 16:47:52 +0800

    Add files via upload

---
+++ easy/7. Reverse Integer.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+        string xToStr = x.ToString();
+        string tmpStr = x < 0 ? xToStr.Substring(1, xToStr.Length - 1) : xToStr;
+        char[] resultCharList = tmpStr.ToCharArray();
+        Array.Reverse(resultCharList);
+        string resultnums = new string(resultCharList);
+        int resultInt = 0;
+        if (int.TryParse(resultnums, out resultInt))
+            return x < 0 ? int.Parse("-" + resultnums) : int.Parse(resultnums);
+        else
+            return 0;
+    }
+}
