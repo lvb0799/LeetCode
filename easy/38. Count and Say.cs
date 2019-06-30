beb00351ce7e2d1f70a5809f1b76aedecb9618c8
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-06-30 18:14:48 +0800

    Add files via upload

---
+++ easy/38. Count and Say.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        if (n == 1)
+            return "1";
+        else
+        {
+            string recursiveString = CountAndSay(n - 1);
+            char[] tmpchar = recursiveString.ToCharArray();
+            Console.WriteLine(recursiveString);
+            int count = 1;
+            string result = string.Empty;
+            for (int i = 0; i < tmpchar.Length; i++)
+            {
+                if (i == tmpchar.Length - 1)
+                    result += count.ToString() + tmpchar[i];
+                else if (tmpchar[i] != tmpchar[i + 1])
+                {
+                    result += count.ToString() + tmpchar[i];
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            return result;
+        }
+    }
+}
