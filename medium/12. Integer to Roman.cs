aa537a2819af877735eded86e537ee088ac5069e
lvb0799 <34149994+lvb0799@users.noreply.github.com>
2019-07-04 00:45:11 +0800

    Add files via upload

---
+++ medium/12. Integer to Roman.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public string IntToRoman(int num)
+    {
+        if (num >= 1000)
+            return 'M' + IntToRoman(num - 1000);
+        else if (num >= 500)
+            return (num >= 900) ? "CM" + IntToRoman(num - 900) : 'D' + IntToRoman(num - 500);
+        else if (num >= 100)
+            return (num >= 400) ? "CD" + IntToRoman(num - 400) : 'C' + IntToRoman(num - 100);
+        else if (num >= 50)
+            return (num >= 90) ? "XC" + IntToRoman(num - 90) : 'L' + IntToRoman(num - 50);
+        else if (num >= 10)
+            return (num >= 40) ? "XL" + IntToRoman(num - 40) : 'X' + IntToRoman(num - 10);
+        else if (num >= 5)
+            return (num == 9) ? "IX" + IntToRoman(num - 9) : 'V' + IntToRoman(num - 5);
+        else if (num >= 1)
+            return (num == 4) ? "IV" + IntToRoman(num - 4) : 'I' + IntToRoman(num - 1);
+        else
+            return "";
+    }
+
+}
